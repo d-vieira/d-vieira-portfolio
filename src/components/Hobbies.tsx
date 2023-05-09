85de50766df109ae34d1cde49748b45e0f2c8a3b
Daniel Vieira <dann-vieira@hotmail.com>
2023-05-09 09:03:40 -0300

    add: Hobbies component

---
+++ src/components/Hobbies.tsx
@@ -0,0 +1,14 @@
+import MusicIcon from "./icons/MusicIcon";
+
+export default function Hobbies() {
+  return (
+    <div className="text-myDarkGrey border rounded-sm drop-shadow-md shadow-md pb-20 pt-2 w-1/3 grid justify-items-center text-center">
+      <MusicIcon />
+      <h2 className=" font-bold text-3xl">HOBBIES</h2>
+      <p>
+        Música, Jogos, Live Streamings, Lazer em família, <br />
+        Esportes, Podcasts, Séries, Filmes
+      </p>
+    </div>
+  );
+}
