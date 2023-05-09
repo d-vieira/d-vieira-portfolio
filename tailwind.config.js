9aab22eec04b64fddb497419a86c41650a4d5e16
Daniel Vieira <dann-vieira@hotmail.com>
2023-05-09 08:39:53 -0300

    add: color palette and shadow options

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        myDarkGrey: '#3A3A3A',
+        myLightGrey: '#D9D9D9',
+        myGreen: '#BDFF00'
       },
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      }
     },
   },
   plugins: [],
