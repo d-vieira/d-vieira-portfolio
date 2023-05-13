3848524ba15dd32509cfe36c657519e41c0e6f8c
Daniel Vieira <dann-vieira@hotmail.com>
2023-05-13 20:24:09 -0300

    add: New section Skills into the home page

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Portfolio from "@/components/Portfolio";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About />
       <Portfolio />
+      <Skills />
     </main>
   );
 }
