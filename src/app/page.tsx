1d0ca2f3dd7ec70f2de4d0a1dd3c563cf742eb1f
Daniel Vieira <dann-vieira@hotmail.com>
2023-05-19 15:14:37 -0300

    add: Footer component on the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Portfolio from "@/components/Portfolio";
 import Skills from "@/components/Skills";
@@ -10,6 +11,7 @@ export default function Index() {
       <About />
       <Portfolio />
       <Skills />
+      <Footer />
     </main>
   );
 }
