d2e2cff8e02068645a173973cd1856b2c1df4221
Daniel Vieira <dann-vieira@hotmail.com>
2023-05-19 15:13:07 -0300

    add: Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,41 @@
+import { NAV_SECTIONS, SOCIALS } from "@/utils/variables";
+import NavSeeder from "./NavSeeder";
+import Email from "./Email";
+import Phone from "./Phone";
+import Location from "./Location";
+import Socials from "./Socials";
+import DVLogo from "./DVLogo";
+import Copyright from "./Copyright";
+
+export default function Footer() {
+  return (
+    <div id="Contate-me" className="bg-myDarkGrey">
+      <div className="flex">
+        <DVLogo />
+
+        <nav className="mt-28 ml-[6%]">
+          <p title="Menu de navegação" className="text-myGreen font-bold mb-3">
+            NAVEGAÇÃO
+          </p>
+          <ul className="text-white">{NAV_SECTIONS.map(NavSeeder)}</ul>
+        </nav>
+
+        <div className="mt-28 ml-[6%]">
+          <p
+            title="Menu de informações"
+            className="text-myGreen font-bold mb-3"
+          >
+            INFORMAÇÕES
+          </p>
+          <Email />
+          <Phone />
+          <Location />
+        </div>
+
+        <Socials socials={SOCIALS} />
+      </div>
+
+      <Copyright />
+    </div>
+  );
+}
