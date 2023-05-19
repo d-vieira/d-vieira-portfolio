4ea979dc6cb4defbea55f0d57d1200cd7737c21d
Daniel Vieira <dann-vieira@hotmail.com>
2023-05-19 15:17:22 -0300

    add: improves accessibility on the nav options

--- src/components/NavSeeder.tsx
+++ src/components/NavSeeder.tsx
@@ -6,6 +6,7 @@ export default function NavSeeder({ name }: navSeederProps) {
   return (
     <li key={name}>
       <a
+        title={`Ir para: ${name}`}
         href={"/#" + name}
         className="scroll-smooth opacity-100 hover:opacity-50 border-transparent duration-300"
       >
