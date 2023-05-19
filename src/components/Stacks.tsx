26846b654c998ed2a1376448fb89879fb4b62dd0
Daniel Vieira <dann-vieira@hotmail.com>
2023-05-19 15:16:02 -0300

    style: hover effect

--- src/components/Stacks.tsx
+++ src/components/Stacks.tsx
@@ -8,7 +8,7 @@ export default function Stacks({ stacks }: { stacks: StacksProps[] }) {
         return (
           <Link
             key={title}
-            className="bg-myLightGrey rounded-md w-32 h-32 flex flex-col items-center m-4"
+            className="bg-myLightGrey rounded-md w-32 h-32 flex flex-col items-center m-4 hover:scale-110 duration-700"
             href={url}
             title={title}
             target="_blank"
