ee59ecfd6b3fdc7f6962f51f3c05727de6d32a3d
Daniel Vieira <dann-vieira@hotmail.com>
2023-05-22 17:52:58 -0300

    add: implements click outside feature in burger menu

--- src/components/MobileNavMenu.tsx
+++ src/components/MobileNavMenu.tsx
@@ -2,14 +2,33 @@ import { NAV_SECTIONS } from "@/utils/variables";
 import CloseButtonIcon from "./icons/CloseButtonIcon";
 import HamburgerMenuIcon from "./icons/HamburgerMenuIcon";
 import NavSeeder from "./NavSeeder";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function MobileNavMenu() {
   const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLUListElement>(null);
 
   const handleClick = () => {
     setOpen(!open);
   };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (open) {
+      document.addEventListener("click", handleClickOutside);
+    } else {
+      document.removeEventListener("click", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [open]);
 
   return (
     <div>
@@ -20,7 +39,10 @@ export default function MobileNavMenu() {
         {open ? <CloseButtonIcon /> : <HamburgerMenuIcon />}
       </button>
       {open && (
-        <ul className="bg-zinc-800 opacity-90 shadow-gray-500 shadow-[0_0_20px_5px_rgba(0,0,0,0.3)] text-myGreen text-center font-semibold text-3xl w-full h-[480px] z-10 left-0 top-[70px] absolute flex flex-col gap-10 py-9">
+        <ul
+          ref={ref}
+          className="bg-zinc-800 opacity-90 shadow-gray-500 shadow-[0_0_20px_5px_rgba(0,0,0,0.3)] text-myGreen text-center font-semibold text-3xl w-full h-[480px] z-10 left-0 top-[70px] absolute flex flex-col gap-10 py-9"
+        >
           {NAV_SECTIONS.map(NavSeeder)}
         </ul>
       )}
