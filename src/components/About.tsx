2c2a1bfb803d15d9a6b3f806a4c38d435b45d920
Daniel Vieira <dann-vieira@hotmail.com>
2023-05-22 21:32:45 -0300

    style: improves responsive design for mobile on the About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,11 +3,11 @@ import Hobbies from "./Hobbies";
 
 export default function About() {
   return (
-    <div id="Sobre" className="bg-white pb-28">
+    <div id="Sobre" className="bg-white pb-28 max-md:-mt-1">
       <h1 className="text-myGreen font-bold text-5xl text-center mb-8">
         Sobre mim
       </h1>
-      <div className="flex justify-center gap-12">
+      <div className="flex justify-center gap-12 max-md:flex-col max-md:items-center max-md:w-full">
         <Education />
         <Hobbies />
       </div>
--- src/components/Hobbies.tsx
+++ src/components/Hobbies.tsx
@@ -2,9 +2,9 @@ import MusicIcon from "./icons/MusicIcon";
 
 export default function Hobbies() {
   return (
-    <div className="text-myDarkGrey border rounded-sm drop-shadow-md shadow-md pb-20 pt-2 w-1/3 grid justify-items-center text-center">
+    <div className="text-myDarkGrey border rounded-sm drop-shadow-md shadow-md pb-20 pt-2 w-1/3 grid justify-items-center text-center max-md:w-72 max-md:px-5">
       <MusicIcon />
-      <h2 className=" font-bold text-3xl">HOBBIES</h2>
+      <h2 className="font-bold text-3xl">HOBBIES</h2>
       <p>
         Música, Jogos, Live Streamings, Lazer em família, <br />
         Esportes, Podcasts, Séries, Filmes
