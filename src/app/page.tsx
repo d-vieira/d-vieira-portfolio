0af01b60ff354dc847c5554e607d684991a6d014
Daniel Vieira <dann-vieira@hotmail.com>
2023-05-22 21:35:27 -0300

    add: To the top Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Portfolio from "@/components/Portfolio";
 import Skills from "@/components/Skills";
+import ArrowUpIcon from "@/components/icons/ArrowUpIcon";
 
 export default function Index() {
   return (
     <main>
       <Hero />
+      <ArrowUpIcon />
       <About />
       <Portfolio />
       <Skills />
---
+++ src/components/icons/ArrowUpIcon.tsx
@@ -0,0 +1,17 @@
+export default function ArrowUpIcon() {
+  return (
+    <a
+      href="#"
+      className="md:hidden fixed left-[300px] bottom-3 z-auto animate-bounce scroll-smooth"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="64"
+        height="64"
+        viewBox="0 0 24 24"
+      >
+        <path d="M11 8.414V18h2V8.414l4.293 4.293 1.414-1.414L12 4.586l-6.707 6.707 1.414 1.414z"></path>
+      </svg>
+    </a>
+  );
+}
