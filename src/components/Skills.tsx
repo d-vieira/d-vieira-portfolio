6b68946af4c902dc6d0159f68adebb377633c900
Daniel Vieira <dann-vieira@hotmail.com>
2023-05-24 19:35:39 -0300

    style: fix space between the content

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -15,13 +15,13 @@ export default function Skills() {
       >
         Stacks
       </h1>
-      <h2 className="text-myGreen text-xl md:hidden">Linguagens / OPS</h2>
+      <h2 className="text-myGreen text-xl mt-2 md:hidden">Linguagens / OPS</h2>
       <Stacks stacks={STACKS_OVERALL} />
-      <h2 className="text-myGreen text-xl md:hidden">Front-End</h2>
+      <h2 className="text-myGreen text-xl mt-12 mr-2 md:hidden">Front-End</h2>
       <Stacks stacks={STACKS_FRONT_END} />
-      <h2 className="text-myGreen text-xl md:hidden">Back-End</h2>
+      <h2 className="text-myGreen text-xl mt-12 mr-3 md:hidden">Back-End</h2>
       <Stacks stacks={STACKS_BACK_END} />
-      <h2 className="text-myGreen text-xl md:hidden">Testes</h2>
+      <h2 className="text-myGreen text-xl mt-12 mr-2 md:hidden">Testes</h2>
       <Stacks stacks={STACKS_TESTING_LIBS} />
     </div>
   );
