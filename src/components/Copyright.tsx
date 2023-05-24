2e49bff7fef8a4d58a99f22ee8e9131597657f4e
Daniel Vieira <dann-vieira@hotmail.com>
2023-05-24 16:52:14 -0300

    change: Copyright text

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -4,7 +4,7 @@ export default function Copyright() {
     <div className="flex flex-col items-center mt-10">
       <hr className="w-[70%] border-myLightGrey max-md:hidden" />
       <p className="text-myLightGrey font-bold mt-5 mb-5 max-md:mb-0.5 max-md:w-[90%] max-md:text-center ">
-        Copyright © {year} Daniel Vieira Martins. Todos os direitos reservados.
+        Copyright © {year} Daniel Vieira Martins
       </p>
     </div>
   );
