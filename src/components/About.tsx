227c5481b1b82ef883e4e0451649332d95c860f1
Daniel Vieira <dann-vieira@hotmail.com>
2023-05-30 14:58:08 -0300

    style: fix margin

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import Hobbies from "./Hobbies";
 
 export default function About() {
   return (
-    <div id="Sobre" className="bg-white pb-28 max-md:-mt-1">
+    <div id="Sobre" className="bg-white pb-28 -mt-1">
       <h1 className="text-myGreen font-bold text-5xl text-center mb-8">
         Sobre mim
       </h1>
