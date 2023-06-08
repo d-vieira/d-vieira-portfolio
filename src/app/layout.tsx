b549192cb9e5b90836b6545b956557b0dbbf5d81
Daniel Vieira <dann-vieira@hotmail.com>
2023-06-08 16:26:22 -0300

    refactor: improve meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,11 @@ import { Jost } from "next/font/google";
 const jost = Jost({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata = {
-  title: "Daniel Vieira",
-  description: "A web developer portfolio",
+  title: "Daniel Vieira - Web Dev Full Stack",
+  description:
+    "Daniel Vieira - Desenvolvedor Web Full Stack. Confira meu portfólio e veja alguns de meus projetos e soluções! [JS, TS, Node, React, CSS, HTML, SQL e NoSQL]",
   icons: [{ rel: "icon", url: "/icons8-dev-96-final.png", sizes: "any" }],
+  robots: "index, follow",
 };
 
 export default function RootLayout({
