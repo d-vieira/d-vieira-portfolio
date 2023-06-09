f63381cc464373f83618a397beaa03062808dd63
Daniel Vieira <dann-vieira@hotmail.com>
2023-06-09 16:07:22 -0300

    fix: meta tag twitter-image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
   robots: "index, follow",
   themeColor: { media: "(prefers-color-scheme: dark)", color: "#3A3A3A" },
   twitter: {
-    images: "/icons8-dev-96-final.png",
+    images: "/dv-transparent.png",
     site: "https://danielvieira.vercel.app/",
     title: "Daniel Vieira",
     description:
