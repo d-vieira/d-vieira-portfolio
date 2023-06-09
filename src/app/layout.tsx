68e0fbdad7321f0f169002e1c86130c9cc1c633e
Daniel Vieira <dann-vieira@hotmail.com>
2023-06-09 15:55:43 -0300

    update meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,19 @@ import { Jost } from "next/font/google";
 const jost = Jost({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata = {
-  title: "Daniel Vieira - Web Dev Full Stack",
+  title: "Daniel Vieira",
   description:
-    "Daniel Vieira - Desenvolvedor Web Full Stack. Confira meu portfólio e veja alguns de meus projetos e soluções! [JS, TS, Node, React, CSS, HTML, SQL, NoSQL]",
+    "Desenvolvedor Web Full Stack. Confira meu portfólio e veja alguns de meus projetos e soluções! [JS, TS, Node, React, CSS, HTML, SQL, NoSQL]",
   icons: [{ rel: "icon", url: "/icons8-dev-96-final.png", sizes: "any" }],
   robots: "index, follow",
   themeColor: { media: "(prefers-color-scheme: dark)", color: "#3A3A3A" },
+  twitter: {
+    images: "/icons8-dev-96-final.png",
+    site: "https://danielvieira.vercel.app/",
+    title: "Daniel Vieira",
+    description:
+      "Desenvolvedor Web Full Stack. Confira meu portfólio e veja alguns de meus projetos e soluções! [JS, TS, Node, React, CSS, HTML, SQL, NoSQL]",
+  },
 };
 
 export default function RootLayout({
